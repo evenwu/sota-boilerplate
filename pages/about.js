51bb3da1032b42af09758c774681203c9fc1c011
Even <evenwu@users.noreply.github.com>
2023-01-10 04:06:15 +0800

    layout 更新

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,13 @@
 import Layout from '../components/_layout'
-import LayoutNested from '../components/_layout-nested'
+import LayoutAbout from '../components/_layout-about'
 
 export default function Page() {
   return (
     <>
-      <h1>About page</h1>
+      <h1 className="mb-5 text-lg font-semibold">About page</h1>
+      <p className="text-fuchsia-204 w-fit rounded-full bg-sky-200 px-3 py-1 text-sky-700">
+        要注意的是 layout 也有改變了
+      </p>
     </>
   )
 }
@@ -12,7 +15,7 @@ export default function Page() {
 Page.getLayout = function getLayout(page) {
   return (
     <Layout>
-      <LayoutNested>{page}</LayoutNested>
+      <LayoutAbout>{page}</LayoutAbout>
     </Layout>
   )
 }
