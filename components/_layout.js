e1beaa032e99cc58f7755c5bb623ada4edae1a19
Even <evenwu@users.noreply.github.com>
2023-05-22 13:38:11 +0800

    引入字體的方式改變

--- components/_layout.js
+++ components/_layout.js
@@ -1,3 +1,10 @@
+import localFont from 'next/font/local'
+
+const inter = localFont({
+  src: '../public/fonts/Inter-roman.latin.var.woff2',
+  variable: '--font-inter',
+})
+
 import Head from 'next/head'
 import Navbar from './navbar'
 import Footer from './footer'
@@ -11,7 +18,8 @@ export default function Layout({children}) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="page p-8">
+
+      <div className={`${inter.variable} page p-8 font-sans`}>
         <Navbar />
         <main>{children}</main>
         <Footer />
