efea8404aa9e613122ed40c44127366d9836aa80
andy0131 <andy0131@gmail.com>
2023-05-23 09:41:07 +0800

    加入NextSeo 套件、測試

--- components/_layout.js
+++ components/_layout.js
@@ -8,17 +8,18 @@ const inter = localFont({
 import Head from 'next/head'
 import Navbar from './navbar'
 import Footer from './footer'
+import Seo from './seo'
 
 export default function Layout({children}) {
   return (
     <>
+      <Seo />
       <Head>
         <title>Site Name</title>
         <meta name="description" content="Site Description" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <div className={`${inter.variable} page p-8 font-sans`}>
         <Navbar />
         <main>{children}</main>
